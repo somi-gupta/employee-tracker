617c31ee6f4a7b85c56f25489e39ef74346c7626
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-04-14 22:50:55 -0400

    Created sql file to create db and tables.

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,34 @@
 DROP DATABASE IF EXISTS admin_db;
-CREATE DATABASE admin_dbs;
+CREATE DATABASE admin_db;
 
+USE admin_db;
 
+CREATE TABLE department {
+    id INT NOT NULL,
+    name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
+}
 
+CREATE TABLE role {
+    id INT NOT NULL,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10,4),
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+}
+
+CREATE TABLE employee {
+    id INT NOT NULL,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES employee(id)
+    FOREIGN KEY (manager_id) REFERENCES role(id)  
+}
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
